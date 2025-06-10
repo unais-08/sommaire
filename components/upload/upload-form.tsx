0ade5c416815a236ac1904f0dafe39da5d926ebb
Shaikh <unaissk1234@gmail.com>
2025-06-10 22:48:38 +0530

    implemented zod validation for file upload

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,14 +1,46 @@
 "use client";
 import React from "react";
+import { z } from "zod";
 import UploadFormInput from "@/components/upload/upload-form-input";
+import { fi } from "zod/v4/locales";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, {
+      message: "Invalid File or File not provided",
+    })
+    .refine((file) => file.size <= 20 * 1024 * 1024, {
+      message: "File size must be less than 20MB",
+    })
+    .refine((file) => file.type.startsWith("application/pdf"), {
+      message: "File must be a PDF",
+    }),
+});
 
 export default function UploadForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Handle form submission logic here
     console.log("Form submitted");
+
+
+    // Handle form submission logic here
     const formData = new FormData(e.currentTarget);
     const file = formData.get("file") as File;
+    
+    const validatedFile = schema.safeParse({ file });
+    
+    // Uncomment the line below to see the validated file in the console
+    // console.log("Validated file:", validatedFile);
+    
+    // If the file is not valid, donot proceed with the upload and processing
+    if (!validatedFile.success) {
+      console.error(
+        "Validation failed:",
+        validatedFile.error.flatten().fieldErrors.file?.[0]
+      );
+      return;
+    }
+
     /*
      * validate the file type and size here
      *schame with zod
@@ -18,13 +50,6 @@ export default function UploadForm() {
      * save the summary to the database
      * redirect to the [id] summary page
      */
-
-    if (file) {
-      console.log("File selected:", file);
-      // You can add further processing of the file here, like uploading it to a server
-    } else {
-      console.log("No file selected");
-    }
   };
 
   return (
