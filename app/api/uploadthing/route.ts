ead9ba600f8f9b8ff31a03722f8edee3e778db1f
Unais Shaikh <unais.shaikh.dev@gmail.com>
2025-06-16 12:48:26 +0530

    comment remove from route handler

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -3,7 +3,4 @@ import { ourFileRouter } from "@/app/api/uploadthing/core";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  // Apply an (optional) custom config:
-  // config: { ... },
 });
-// export const dynamic = "force-dynamic"; // Force dynamic rendering for this route
