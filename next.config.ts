76be51bfea2260ab3f2d0356dfa4d03654033d3e
Shaikh <unaissk1234@gmail.com>
2025-06-22 18:02:17 +0530

    uploadthing onUploadSuccess response not give serverData in the production build so I opt for alternate option from upload resoponse :) ,  solve the most irritating bug of my life till

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // Set the body size limit for server actions
+    },
+  },
 };
 
 export default nextConfig;
